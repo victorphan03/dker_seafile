a66827ce658b7b1765305993db094286d9c479e5
x79 <basketballcantho@gmail.com>
2025-05-30 14:18:39 +0700

    done v1

--- 02_backup.sh
+++ 02_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # filepath: /home/x79/seafile/backup.sh
-
+docker compose down
 TITLE=${1:-backup}
 DATE=$(date +"%Y%m%d_%H%M%S")
 BACKUP_FILE="seafile_${TITLE}_${DATE}.tar.gz"
@@ -8,3 +8,4 @@ BACKUP_FILE="seafile_${TITLE}_${DATE}.tar.gz"
 tar -czvf "$BACKUP_FILE" ./data
 
 echo "Backup created: $BACKUP_FILE"
+docker compose up -d
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # dker_seafile
+ghi đè file seahub_setting.py trong docker compose mới chạy được URL bên ngoài
